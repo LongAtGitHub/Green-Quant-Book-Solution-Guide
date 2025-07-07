e9d45009467a6f3c01e7416c1bc2c6ea88fc48bc
LongAtGitHub <98071520+LongAtGitHub@users.noreply.github.com>
2025-07-06 23:20:21 -0500

    MVP

--- my-website/src/components/HomepageFeatures/index.js
+++ my-website/src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Quant Green Companion is awesome',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        This green quant book guide is awesome. It provides a solid reference point for tackling tricky quant and logic problems in a clean, practical style.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'The Author is Awesome',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        The author of this guide site are awesome. He wants to share further insights and solutions more clearly to help you build your problem-solving skills faster.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Check Out More Work of Mine and Me Later',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        If you enjoyed this, make sure to check out more of my work and follow me later for additional resources, updates, and other interesting math and quant explorations.
       </>
     ),
   },
